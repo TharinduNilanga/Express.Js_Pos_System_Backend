15d6bb8e0d6271622f6d38ae216e614fe9b9eeff
nilanaga2 <nilangak107@gmail.com>
2022-08-19 00:16:31 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const user=require('./routes/users')
+const customer=require('./routes/customer')
+const item=require('./routes/item')
+const order=require('./routes/order')
+const orderDetails=require('./routes/orderDetails')
 const app = express();
 const port = 4000;
 
 app.use(express.json())
-app.use('/users',user)
+app.use('/customer',customer)
+app.use('/item',item)
+app.use('/order',order)
+app.use('/orderDetails',orderDetails)
 
 app.listen(port,()=>{
     console.log(`starting on ${port}`);
